b3f2ff8382c0d3cd0e9ee80b58fd3174dfe7281c
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-03-25 15:08:30 +0100

    Finish project

---
+++ src/components/Title.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-center">La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Title",
+};
+</script>
